be44f1a36155b60d42553ee04a67ac00329cdb0f
Samuel Koog <samuel.koog@gmail.com>
2026-02-22 20:44:18 -0800

    Initial Commit

---
+++ PantrySnap/PantrySnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantrySnapApp.swift
+//  PantrySnap
+//
+//  Created by Sam Koog on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PantrySnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
